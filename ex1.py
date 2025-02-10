b05daed68d22ca05703885ac7e4be632de3f264c
cleipoldt <ccpleipoldt@gmail.com>
2025-02-10 10:28:39 -0700

    Finished

--- ex1.py
+++ ex1.py
@@ -10,21 +10,23 @@ When it gets divided into two subproblems, the subproblems share certain overlap
 Q3. The expression for the time complexity is T(n) is O(2^n)
 
 """
+import matplotlib.pyplot as plt
+import timeit
 
-"""
+
+# this function does not use memoization
+
 def func(n):
     if n == 0 or n == 1:
         return n
     else:
         return func(n-1) + func(n-2)
-    
 
-for i in range(10):
-    print(func(i))
+
+
 """
-
-# Q4 Fibonacci implementation which uses Memoization
-
+Q4 Fibonacci implementation which uses Memoization
+"""
 cache = {0: 0, 1: 1}
 
 def fibonacci(n):
@@ -34,4 +36,45 @@ def fibonacci(n):
     cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
     return cache[n]
 
-print([fibonacci(n) for n in range(15)])
+
+"""
+Q5. The time complexity is now linear, T(n) is O(n)
+"""
+
+all_times_fast = []
+for i in range(35):
+    exec_time_fast = timeit.timeit(lambda: fibonacci(i), number = 1)
+    all_times_fast.append(exec_time_fast)
+
+all_times_slow = []
+
+for i in range(35):
+    exec_time_slow = timeit.timeit(lambda: func(i), number = 1)
+    all_times_slow.append(exec_time_slow)
+
+
+x_axis_fast = list(range(35))
+plt.figure(figsize = (10, 5))
+plt.plot(x_axis_fast, all_times_fast, marker='s', linestyle='--', color='r')
+
+plt.xlabel("All integers from 0 to 35")
+plt.ylabel("Execution times (secs)")
+plt.title("The execution times for each fibonnaci number up to 35 using memoization")
+plt.grid(True)
+plt.savefig("ex1.6.2.jpg", dpi=300, bbox_inches='tight')
+plt.show()
+plt.close()
+
+x_axis_slow = list(range(35))
+plt.figure(figsize = (10, 5))
+plt.plot(x_axis_slow, all_times_slow, marker='s', linestyle='-', color="g")
+plt.xlabel("All integers from 0 to 35")
+plt.ylabel("Execution times (secs)")
+plt.title("The execution times for each fibonnaci number up to 35 not using memoization")
+plt.grid(True)
+plt.savefig("ex1.6.1.jpg", dpi=300, bbox_inches='tight')
+plt.show()
+plt.close()
+
+
+
